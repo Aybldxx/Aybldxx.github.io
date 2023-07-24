2f9606a2107207e19ad844107cb84f5f8e0cadbf
Aybldxx <140290823+Aybldxx@users.noreply.github.com>
2023-07-24 19:49:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const menuIcon = document.querySelector('.menu-icon');
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+
+menuIcon.addEventListener('click', toggleMenu);
+
 function toggleMenu() {
-  const menu = document.getElementById('hamburgerMenu');
-  menu.classList.toggle('active');
+  hamburgerMenu.classList.toggle('active');
 }
- 
