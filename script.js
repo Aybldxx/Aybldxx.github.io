3f96981ebc86905f7bc28ba7f9ce554b021bb0ea
Aybldxx <140290823+Aybldxx@users.noreply.github.com>
2023-07-24 20:17:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const hamburgerMenu = document.getElementById('hamburgerMenu');
-
-menuIcon.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-  hamburgerMenu.classList.toggle('active');
-} 
